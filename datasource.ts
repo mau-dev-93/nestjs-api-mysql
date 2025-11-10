8e747b8d315e2b029b3c4492c7d8888f495ee1a5
Mauricio Muñoz <hmmunozf@gmail.com>
2025-11-09 20:26:40 -0600

    refactor: improve MySQL port configuration by ensuring it is a number

--- datasource.ts
+++ datasource.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
 	type: 'mysql',
 	host: process.env.MYSQL_HOST,
-	port: process.env.MYSQL_PORT,
+	port: Number(process.env.MYSQL_PORT),
 	username: process.env.MYSQL_USERNAME,
 	password: process.env.MYSQL_PASSWORD,
 	database: process.env.MYSQL_DATABASE,
